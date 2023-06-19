cbcb6c8a7ad54e1ee8dd5c7526fdbcaf9da8576f
Revenciel <clarkybox@gmail.com>
2023-06-19 09:36:55 -0400

    layout component not working

---
+++ tina/config.ts
@@ -0,0 +1,85 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: null, // Get this from tina.io
+  token: null, // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "page",
+        label: "Pages",
+        path: "src/content/pages",
+        ui: {
+          router: ({ document }) => {
+            if (document._sys.filename === "home") {
+              return `/`;
+            }
+            return "/" + document._sys.filename;
+          },
+        },
+        fields: [
+          {
+            type: "string",
+            label: "Title",
+            name: "title",
+            description: "The title of the page.",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: 'string',
+            name: 'body',
+            label: 'Body text',
+            isBody: true,
+
+            // type: "object",
+            // list: true,
+            // name: "blocks",
+            // label: "Sections",
+            // ui: {
+            //   visualSelector: true,
+            // },
+            // templates: [
+            //   // add templates here
+            // ],
+          },
+        ],
+      },
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
