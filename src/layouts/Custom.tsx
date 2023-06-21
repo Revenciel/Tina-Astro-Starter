f984ea78ece1440ba79c808e6b8b8dc9d2378f65
Revenciel <clarkybox@gmail.com>
2023-06-20 22:14:05 -0400

    fixed type error on Custom.tsx

--- src/layouts/Custom.tsx
+++ src/layouts/Custom.tsx
@@ -1,27 +1,31 @@
-export default function Layout(props) {
+
+//import { useTina } from 'tinacms/dist/react';
+
+import type { ReactNode } from "react";
+
+interface props {
+    title: string;
+    children: ReactNode;
+}
+
+// what is a "ReactNode"? I put that as the type because of a VS code error that I didn't understand
+
+export default function Layout({title, children}: props) {
+//export default function Layout(props: any) {
+
+    // const { data } = useTina({
+    //     query: props.query,
+    //     variables: props.variables,
+    //     data: props.data,
+    //   })
+
     return (
         <html lang="en">
             <head></head>
             <body>
-                <h1>{props.title}</h1>
-                <main>{props.children}</main>
+                <h1>{title}</h1>
+                <main>{children}</main>
             </body>
         </html>
     );
 };
-
-// export interface Props {
-//     title: string;
-//   }
-
-// export default function Layout({title}: Props) {
-//     return (
-//         <html lang="en">
-//             <head></head>
-//             <body>
-//                 <h1>{title}</h1>
-//                 <slot />
-//             </body>
-//         </html>
-//     );
-// };
